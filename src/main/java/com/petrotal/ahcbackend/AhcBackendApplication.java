356d2c977c4945bbdb63da5992d90d3d5270a9a6
Alan Loyola <alanloyola2002@gmail.com>
2024-05-03 09:19:57 -0500

    Initial Commit

---
+++ src/main/java/com/petrotal/ahcbackend/AhcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petrotal.ahcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petrotal/ahcbackend/AhcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petrotal.ahcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
